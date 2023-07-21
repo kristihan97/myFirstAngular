dca044b1285cd3e23aae4fb4fca7c480aecb886f
Kristian Hansen <kristian.hansen@gintel.com>
2023-07-21 09:55:56 +0200

    fixedAnyIssues

--- src/app/exercise-form/exercise-form.component.ts
+++ src/app/exercise-form/exercise-form.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiGetterService } from '../api-getter.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { validateBasis } from '@angular/flex-layout';
 import { MatStepper } from '@angular/material/stepper';
+
+interface Selections {
+  firstCtrl?: string;
+  secCtrl?: string;
+}
+
+interface ExerciseData {
+  forces: string[];
+  categories: string[];
+  [key: string]: string | string[];
+}
 
 @Component({
   selector: 'app-exercise-form',
@@ -10,53 +20,54 @@ import { MatStepper } from '@angular/material/stepper';
   styleUrls: ['./exercise-form.component.scss'],
 })
 export class ExerciseFormComponent implements OnInit {
-  [key: string]: any;
-  data: any;
-  forces: any;
-  categories: any;
-  selected: any;
-  selections: any = {};
+  data: ExerciseData | undefined;
+  forces: string[] | undefined;
+  categories: string[] | undefined;
+  selected: string | undefined;
+  selections: Selections = {};
 
-  firstCtrl = this.formBuilder.group({
-    //firstStep: ['', Validators.required],
-    firstStep: ['', Validators.required],
-  });
-
-  secCtrl = this.formBuilder.group({
-    secStep: ['', Validators.required],
-  });
+  firstCtrl: FormGroup;
+  secCtrl: FormGroup;
   isEditable = false;
 
   constructor(
     private apiGetter: ApiGetterService,
     private formBuilder: FormBuilder
-  ) {}
+  ) {
+    this.firstCtrl = this.formBuilder.group({
+      firstStep: ['', Validators.required],
+    });
+
+    this.secCtrl = this.formBuilder.group({
+      secStep: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
-    this.apiGetter.getData().subscribe((data) => {
+    this.apiGetter.getData().subscribe((data: ExerciseData) => {
       this.data = data;
-      this.forces = data['forces'];
-      this.categories = data['categories'];
-      // console.log(data);
+      this.forces = data.forces;
+      this.categories = data.categories;
       console.log(typeof this.categories);
       console.log(Array.isArray(this.forces));
     });
 
-    this.firstCtrl.get('firstStep')!.valueChanges.subscribe((value) => {
+    this.firstCtrl.get('firstStep')!.valueChanges.subscribe((value: string) => {
       console.log('First step value changed: ', value);
     });
 
-    this.secCtrl.get('secStep')!.valueChanges.subscribe((value) => {
+    this.secCtrl.get('secStep')!.valueChanges.subscribe((value: string) => {
       console.log('Second step value changed: ', value);
     });
-    console.log(this);
   }
-  nextStep(stepper: MatStepper, controlName: string): void {
+
+  nextStep(stepper: MatStepper, controlName: keyof Selections): void {
     const group = this[controlName] as FormGroup;
     const controlNames: { [key: string]: string } = {
       firstCtrl: 'firstStep',
       secCtrl: 'secStep',
     };
+
     if (group.valid) {
       this.selections[controlName] = group.get(
         controlNames[controlName]
